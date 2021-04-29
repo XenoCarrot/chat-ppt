ed92f8a1b04d950214349de465380b462b6781c7
XenoCarrot <victorgabri932@gmail.com>
2021-04-29 01:17:18 -0300

    k

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,6 @@
 function Home(){
-    return (
-        <body>
+    
 
-            <div>
-
-                <h1>Bem vindo ao futuro Linie.</h1>
-                <p>Este é o site que será usado no futuro para fazer os chats de forma segura</p>
-                <button id="button" onClick={console.log("Deu certo")}>Clica aí</button>
-
-            </div>
-
-            <script>
-
-                function name() {
-                    console.log("K")
-                }
-
-            </script>
-
-        </body>
-    );
     
 }
 

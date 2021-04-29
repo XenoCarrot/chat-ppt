7151fce554e236859d1210b57eb61f64e9df5626
XenoCarrot <victorgabri932@gmail.com>
2021-04-29 00:24:11 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 function Home(){
     return (
         <div>
-            
+
             <h1>Bem vindo ao futuro Linie.</h1>
             <p>Este é o site que será usado no futuro para fazer os chats de forma segura</p>
         
         </div>
     )
+}
+
+function test(){
+    console.log("Deu certo!")
 }
 
 export default Home

09f17aaa09464b118a90539881db57d38eab63a6
XenoCarrot <victorgabri932@gmail.com>
2021-04-29 00:50:18 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,9 @@ function Home(){
 
                 <h1>Bem vindo ao futuro Linie.</h1>
                 <p>Este é o site que será usado no futuro para fazer os chats de forma segura</p>
-                <button id="button">Clica aí</button>
+                <button id="button" onClick={alert("Deu certo")}>Clica aí</button>
 
             </div>
-
-            <script>
-                document.getElementById("button").addEventListener('click', function run(){test()})
-            </script>
 
         </body>
     );
